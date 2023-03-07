387c592da1b9643c07f6259946d176aeb3b2ee55
TGusciora <tgusciora@gmail.com>
2023-03-07 12:41:12 +0100

    2023/03/07 update
    GP23
    - MakeDataset class preparation for data import

--- GP23_PredictingHouseSalePrices/gp23package/data/make_dataset.py
+++ GP23_PredictingHouseSalePrices/gp23package/data/make_dataset.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import os
 
-def make_dataset(file_name = 'AmesHousing.tsv', dlm = "\t"):
 
-    absolute_path = os.path.abspath(os.path.join(__file__ ,"../../.."))
-    print(absolute_path)
-    relative_path = "\\data\\raw"
-    print(relative_path)
-    data_dir = os.path.join(absolute_path, relative_path)
-    print(data_dir)
-    full_path = os.path.join(data_dir, file_name)
-    print(full_path)
-    #data = pd.read_csv(path = full_path, delimiter = dlm)
-    #return data
+class MakeDataset:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.data = import_dataset(self.file_name)
+
+    def import_dataset(file_name='AmesHousing.tsv', dlm="\t"):
+        """ Create dataset (pandas DataFrame) for analysis
+
+        :param file_name: raw source file to import
+        :param dlm: delimiter used to separate columns in raw source file
+
+        :return: pandas DataFrame representing imported raw source file
+        """
+        # Return main package / prooject directory
+        absolute_path = os.path.abspath(os.path.join(__file__, "../../.."))
+        # Subdirectory with raw data
+        relative_path = "\\data\\raw\\"
+        # Establish full path to raw file
+        full_path = absolute_path + relative_path + file_name
+        # Import raw file as data pandas DataFrame
+        data = pd.read_csv(full_path, delimiter=dlm)
+        return data
+
 
 data = make_dataset()
