ccd338ba49f5fc5ae40694db78c2fc805867a304
TGusciora <tgusciora@gmail.com>
2023-04-15 10:41:33 +0200

    2023/04/15
    GP23
    - diagnostics further refactoring (nearly ready)

--- GP23_PredictingHouseSalePrices/gp23package/utils/diagnostics.py
+++ GP23_PredictingHouseSalePrices/gp23package/utils/diagnostics.py
@@ -33,24 +33,24 @@ class LinearDiagnostics():
     * Variance Inflation Factor (VIF) table \n
     * Correlation matrix of independent variables
 
-    Parameters
+    Attributes
     ----------
-    model_in : str
-        Name of model stored in models_list dictionary
-    data_in : str
-        Dataset on which predictions will be made and residuals calculated.
-        Must have variables required by the model present.
-    target : str
+    model: str
+        Name of model used for diagnostics.
+    data : str
+        Data used for model goodness of fit.
+    target : series
         Series of target variable value. Must have corresponding index value
-        with data_in.
-    scoring_dict : str
-        Stored dictionary with list of variables selected for scoring.
-    model_list : str
-        Stored dictionary with list of estimated models.
-    significance_level : float, default = 0.05
+        with data.
+    alpha : int
         Probability of rejecting H0 when it is true. Parameter for statistical
         tests. Against significance level we find critical value for
-        statistical test and compare it to statistic.
+        statistical test and compare it to statistic. Equal to
+        significance_level parameter.
+    predictions : pandas DataFrame
+        DataFrame with predicted values of dependent variable.
+    results_df : pandas DataFrame
+        DataFrame with goodness of fit for each observation.
 
     Notes
     -------------------
@@ -70,6 +70,43 @@ class LinearDiagnostics():
     * import seaborn as sn \n
     * import pandas as pd
     * import numpy as np
+
+        self._residual_stats()
+        self._res_kde()
+        self._res_qq()
+        self._jb_normal_test()
+
+    Methods
+    -------
+    __init__(self, model_in, data_in, target, scoring_dict, model_list, significance_level=0.05)
+        Constructor method.
+    _residual_stats(self)
+        Print descriptive statistics of residuals.
+    _res_hist(self)
+        Display residuals histogram.
+    _res_kde(self)
+        Display residuals kernel density plot.
+    _jb_normal_test(self)
+        Perform Jarque-Bera test for normality of residuals.
+    _sw_normal_test(self)
+        Perform Shapiro-Wilk test for normality of residuals.
+    _ad_normal_test(self)
+        Perform Anderson-Darling test for normality of residuals.
+    _normal_test(self)
+        Perform D'Agostino normality test for residuals.
+    _dw_autocorr_test(self)
+        Perform Durbin-Watson test for autocorrelation of residuals.
+    _homoskedasticity_plot(self)
+        Display predicted values vs residuals plot.
+    _w_test_homoskedasticity(self)
+        Perform White test for homoskedasticity.
+    _bp_test_homoskedasticity(self)
+        Perform Breusch-Pagan test for homoskedasticity.
+    _vif(self)
+        Display variance inflation factors (VIF).
+    _corr_matrix(self)
+        Display Pearson correlation matrix between variables.
+
     """
 
     def __init__(self, model_in, data_in, target, scoring_dict, model_list,
@@ -94,9 +131,9 @@ class LinearDiagnostics():
         model_list : str
             Stored dictionary with list of estimated models.
         significance_level : float, default = 0.05
-            Probability of rejecting H0 when it is true. Parameter for statistical
-            tests. Against significance level we find critical value for
-            statistical test and compare it to statistic.
+            Probability of rejecting H0 when it is true. Parameter for
+            statistical tests. Against significance level we find
+            critical value for statistical test and compare it to statistic.
         """
         self.model = model_in
         self.data = data_in
@@ -109,51 +146,74 @@ class LinearDiagnostics():
         self.results_df = pd.DataFrame()
         self.results_df['predicted'] = list(self.predictions)
         self.results_df['actual'] = list(self.target)
-        self.results_df['residual'] = self.results_df['predicted'] - self.results_df['actual']
+        self.results_df['residual'] = self.results_df['actual'] - self.results_df['predicted']
         self.results_df = self.results_df.sort_values(by='residual').reset_index(drop=True)
-        self.residual_stats()
-        self.res_kde()
-        self.res_qq()
-        self.jb_normal_test()
-        self.sw_normal_test()
-        self.ad_normal_test()
-        self.normal_test()
-        self.dw_autocorr_test()
-        self.homoskedasticity_plot()
-        self.w_test_homoskedasticity()
-        self.bp_test_homoskedasticity()
-        self.vif()
-        self.corr_matrix()
+        self._residual_stats()
+        self._res_hist()
+        self._res_kde()
+        self._res_qq()
+        self._jb_normal_test()
+        self._sw_normal_test()
+        self._ad_normal_test()
+        self._normal_test()
+        self._dw_autocorr_test()
+        self._homoskedasticity_plot()
+        self._w_test_homoskedasticity()
+        self._bp_test_homoskedasticity()
+        self._vif()
+        self._corr_matrix()
 
-    def residual_stats(self):
+    def _residual_stats(self):
         """
         Calculate residual descriptive statistics on pointed dataset.
 
-        Calculating summary statistics about residuals (true - predicted)
+        Calculating summary statistics about residuals (true - predicted
+        values)
         values of dependent variable.
         """
         print('*** Test data residuals statistics ***')
         display(HTML(self.results_df.describe().to_html()))
 
-    def res_kde(self):
+    def _res_hist(self):
         """
         Display residuals histogram.
+
+        Displays histogram of residuals (true values - predicted values).
         """
         sn.histplot(data=self.results_df['residual'])
         pylab.title('Residuals histogram')
         plt.show()
 
-    def res_qq(self):
+    def _res_kde(self):
+        """
+        Display residuals kernel density plot.
+
+        Displays kernel density plot of residuals (true values - predicted
+        values).
+        """
+        sn.kdeplot(data=self.results_df['residual'])
+        pylab.title('Test data residuals kernel density plot')
+        plt.show()
+
+    def _res_qq(self):
         """
         Display residuals qqplot.
+
+        Display residuals (true values - predicted values) quantile-quantile 
+        plot.
         """
         stats.probplot(self.results_df['residual'], dist="norm", plot=pylab)
         pylab.title('Residuals qq plot')
         plt.show()
 
-    def jb_normal_test(self):
+    def _jb_normal_test(self):
         """
-        Perform Jarque Bera test for normality of residuals
+        Perform Jarque Bera test for normality of residuals.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Jarque%E2%80%93Bera_test> \n
         """
         print('*** Jarque-Bera test for normality of residuals ***')
         name = ['Jarque-Bera', 'Chi^2 two-tail prob.', 'Skew', 'Kurtosis']
@@ -166,12 +226,15 @@ class LinearDiagnostics():
             print('On', self.significance_level, 'significance level we ',
                   'reject H0 about normal distribution of residuals.')
         print()
-    
-    def sw_normal_test(self):
+
+    def _sw_normal_test(self):
         """
         Perform Shapiro-Wilk test for normality of residuals.
 
-
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Shapiro%E2%80%93Wilk_test> \n
         """
     # 'Shapiro-Wilk test for normality'
         print('Shapiro-Wilk test for normality')
@@ -184,9 +247,14 @@ class LinearDiagnostics():
                   'residuals come from a normal distribution.')
         print()
 
-    def ad_normal_test(self):
+    def _ad_normal_test(self):
         """
-        
+        Perform Anderson-Darling test for normality of residuals.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Anderson%E2%80%93Darling_test> \n
         """
         # Anderson-Darling Test for normality
         # H0 : sample is drawn from a population that follows a 
@@ -207,11 +275,16 @@ class LinearDiagnostics():
                       'distribution.' % (sl, cv))
         print()
 
-    def normal_test(self):
+    def _normal_test(self):
         """
-        
+        Perform D'Agostino normality test.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/D%27Agostino%27s_K-squared_test> \n
         """
-        # Normal test    
+        # Normal test
         print('Normal test for normality')
         stat, p = stats.normaltest(self.results_df['residual'])
         print('Statistics=%.3f, p=%.3f' % (stat, p))
@@ -224,9 +297,14 @@ class LinearDiagnostics():
                   'residuals come from a normal distribution.')
         print()
 
-    def dw_autocorr_test(self):
+    def _dw_autocorr_test(self):
         """
-        
+        Perform Durbin-Watson test for autocorrelation.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Durbin%E2%80%93Watson_statistic> \n
         """
         print('*** Durbin-Watson residual autocorrelation test ***')
         durbin = sm.stats.stattools.durbin_watson(self.results_df['residual'])
@@ -242,8 +320,11 @@ class LinearDiagnostics():
                   'between residuals.')
         print()
 
-    def homoskedasticity_plot(self):
+    def _homoskedasticity_plot(self):
         """
+        Plot predicted values against residual errors.
+
+        Investigate visually possible homoscedasticity.
         """
         print('*** Investigating homoscedasticity of residuals ***')
         # Plot the model's residuals against the predicted values of y
@@ -266,10 +347,15 @@ class LinearDiagnostics():
                   max(self.results_df['actual'].max(),
                   self.results_df['predicted'].max())], color='black')
         plt.show()
-    
-    def w_test_homoskedasticity(self):
+
+    def _w_test_homoskedasticity(self):
         """
-        
+        Perform White test for homoskedasticity.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/White_test> \n
         """
         print('*** White test for Homoscedasticity ***')
         # perform White's test
@@ -292,9 +378,14 @@ class LinearDiagnostics():
                   'assume heteroscedasticity of residuals.')
         print()
 
-    def bp_test_homoskedasticity(self):
+    def _bp_test_homoskedasticity(self):
         """
-        
+        Perform Breusch-Pagan test for homoskedasticity.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Breusch%E2%80%93Pagan_test> \n
         """
         print('*** Breusch-Pagan homoscedasticity test***')
         name = ['Lagrange multiplier statistic', 'p-value',
@@ -312,32 +403,43 @@ class LinearDiagnostics():
         print()
         # Either one can transform the variables to improve the model, or
         # use a robust regression method
-        # that accounts for the heteroscedasticity. 
-    
-    def vif(self):
-        """
-        
-        """
-    # Multicollinearity check
-    print('*** Investigating Multicollinearity ***')
-    print('*** Variance Inflation Factor (VIF) table***')
-    vif_data = pd.DataFrame()
-    vif_data["feature"] = self.prdct_set.columns
-    # calculating VIF for each feature
-    vif_data["VIF"] = [variance_inflation_factor(self.rdct_set.values, i)
-                       for i in range(len(self.prdct_set.columns))]
-    display(HTML(vif_data.to_html()))
-    if vif_data["VIF"].max() > 5:
-        print('There is indication (VIF > 5) that multicollinearity is ',
-              'present in the data.')
-    else:
-        print('Based on VIF there seems that there are no significant ',
-              'correlations between independent variables.')
-    print()
+        # that accounts for the heteroscedasticity.
 
-    def corr_matrix(self):
+    def _vif(self):
         """
-        
+        Display variance inflation factors (VIF).
+
+        Calculate variance inflation factors in dataset to check for
+        multicollinearity.
+
+        References
+        ----------
+        Source materials: \n
+        1. Wiki <https://en.wikipedia.org/wiki/Variance_inflation_factor> \n
+        """
+        # Multicollinearity check
+        print('*** Investigating Multicollinearity ***')
+        print('*** Variance Inflation Factor (VIF) table***')
+        vif_data = pd.DataFrame()
+        vif_data["feature"] = self.prdct_set.columns
+        # calculating VIF for each feature
+        vif_data["VIF"] = [variance_inflation_factor(self.prdct_set.values, i)
+                           for i in range(len(self.prdct_set.columns))]
+        display(HTML(vif_data.to_html()))
+        if vif_data["VIF"].max() > 5:
+            print('There is indication (VIF > 5) that multicollinearity is ',
+                  'present in the data.')
+        else:
+            print('Based on VIF there seems that there are no significant ',
+                  'correlations between independent variables.')
+        print()
+
+    def _corr_matrix(self):
+        """
+        Display Pearson correlation matrix between variables.
+
+        Calculates and displays matrix of correlations between variables in
+        dataset. Correlation metric - Pearson correlation.
         """
         print('*** Correlation Matrix ***')
         f, ax = plt.subplots(figsize=(40, 30))
